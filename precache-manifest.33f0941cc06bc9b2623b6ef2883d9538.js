5a4caf83ef931b3b9f4db26a2ff7bbcc8ec2ef9b
niikitabhatt <nikitabhatt099@gmail.com>
2020-09-22 13:09:15 +0530

    Updates

---
+++ precache-manifest.33f0941cc06bc9b2623b6ef2883d9538.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22a855338a011f0118a6dbc3faa8ce5d",
+    "url": "/react-sidebar-menu/index.html"
+  },
+  {
+    "revision": "c5b9182970b2d46c79af",
+    "url": "/react-sidebar-menu/static/css/main.999e954a.chunk.css"
+  },
+  {
+    "revision": "a09070a790fa24f9bca9",
+    "url": "/react-sidebar-menu/static/js/2.27bc9cab.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-sidebar-menu/static/js/2.27bc9cab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5b9182970b2d46c79af",
+    "url": "/react-sidebar-menu/static/js/main.fafed506.chunk.js"
+  },
+  {
+    "revision": "a6988ecc70d5fc23e693",
+    "url": "/react-sidebar-menu/static/js/runtime-main.d696f449.js"
+  }
+]);
